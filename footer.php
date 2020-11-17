f394ba628504881526fa726c8a22da45fbbba074
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-11-17 11:58:56 +0300

    added footer

---
+++ footer.php
@@ -0,0 +1,65 @@
+    <footer class="footer">
+        <div class="footer-info">
+            <div class="footer-info-block">
+                <h6 class="footer-title">О нашем магазине</h6>
+                <p class="footer-info-body">
+                    Сайт дверей "ПрофильДорс СПб" - это гарантия качества и выгодных цен. Среди основных преимуществ можно выделить:
+
+                    широкий ассортимент. У нас представлены все модели и актуальные цены на двери
+                    привлекательные цены. Возможность купить без посредников по самой низкой цене
+                    гарантия качества. Наличие всех сертификатов и гарантийных талонов
+                    высокое сервисное обслуживание
+                    полный комплекс услуг, от замера проёмов до установки дверей.
+                </p>
+            </div>
+            <div class="footer-info-block">
+                <h6 class="footer-title">Рейтинг/Награды</h6>
+                <p class="footer-info-body">
+                    <img src="img/rating_4_1.png" alt="">
+                    <img src="img/vybor-polzovatelej.jpg" alt="">
+                </p>
+            </div>
+        </div>
+        <div class="footer-contacts">
+            <span class="footer-contacts-phone"></span>
+            <span class="footer-contacts-working"></span>
+            <span class="footer-contacts-email"></span>
+            <ul>
+                <li class="footer-contacts-address"></li>
+            </ul>
+            <div class="footer-contacts-social">
+                <p class="footer-contacts-social-messengers">
+                    <h6 class="footer-title">Пишите нам в чат</h6>
+                    <ul>
+                        <li><a href="" class="social-icon social-icon_whatsapp"></a></li>
+                        <li><a href="" class="social-icon social-icon_telegram"></a></li>
+                        <li><a href="" class="social-icon social-icon_viber"></a></li>
+                    </ul>
+                </p>
+                <p class="footer-contacts-social-webs">
+                    <h6 class="footer-title">Мы в социальных сетях</h6>
+                    <ul>
+                        <li><a href="" class="social-icon social-icon_instagram"></a></li>
+                        <li><a href="" class="social-icon social-icon_vk"></a></li>
+                        <li><a href="" class="social-icon social-icon_facebook"></a></li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+        <div class="footer-map">
+            <img src="img/map.png" alt="">
+        </div>
+        <div class="footer-bottom">
+            <p class="footer-payments"></p>
+            <div class="footer-additional">
+                <p class="footer-additional-copyright">
+                    © Межкомнатные и входные двери от производителя ПрофильДорс СПб
+                    Сайт дверей ProfilDoors SPb 2010-2020
+                    Не является публичной офертой
+                </p>
+                <a href="" class="footer-additional-sitemap">Карта сайта</a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+<header class="header">
+    <div class="header-top">
+        <div class="header-wrapper">
+            <div class="topMenu">
+                <ul class="topMenu-left">
+                    <li><a class="topMenu-link" href="limit:8080">Двери оптом</a></li>
+                    <li><a class="topMenu-link" href="limit:8080">Доставка</a></li>
+                    <li><a class="topMenu-link" href="limit:8080">Установка</a></li>
+                    <li><a class="topMenu-link" href="limit:8080">Замер</a></li>
+                    <li><a class="topMenu-link" href="limit:8080">Вакансии</a></li>
+                </ul>
+                <ul class="topMenu-right">
+                    <li>
+                        <img class="topMenu-icon" src="icons/003-avatar.svg" alt="">
+                        <a class="topMenu-link" href="limit:8080">Написать директору</a>
+                    </li>
+                    <li>
+                        <img class="topMenu-icon" src="icons/006-pen.svg" alt="">
+                        <a class="topMenu-link" href="limit:8080">Консультация дизайнера</a>
+                    </li>
+                    <li>
+                        <img class="topMenu-icon" src="icons/008-checked.svg" alt="">
+                        <a class="topMenu-link" href="limit:8080">Наши работы</a
+                    </li>
+                    <li>
+                        <img class="topMenu-icon" src="icons/007-placeholder.svg" alt="">
+                        <a class="topMenu-link" href="limit:8080">Где купить</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="header-main">
+        <div class="header-wrapper">
+            <div class="header-info">
+                <p class="header-logo">
+                    <img class="header-logo-img" src="./img/logo.png">
+                    <span class="header-logo-info">Официальный сайт фирменной сети Профиль Дорс</span>
+                </p>
+                <p class="header-info-item">
+                    <img class="header-info-item-logo" src="icons/001-phone-call.svg">
+                    <span class="header-info-item-title header-info-item-title_phone">+7 (812) 209-04-25</span>
+                    <span class="header-info-item-text">Единый контактный центр</span>
+                </p>
+                <p class="header-info-item">
+                    <img class="header-info-item-logo"  src="icons/004-clock.svg">
+                    <span class="header-info-item-title">ПН - ВС: 10:00 - 20:00</span>
+                    <span class="header-info-item-text">Без обеда</span>
+                </p>
+            </div>
+            <div class="header-cart">
+                <span class="header-cart-text">0 товар(ов)</span>
+                <img class="header-cart-logo" src="icons/002-shopping-cart-empty-side-view.svg">
+            </div>
+        </div>
+
+    </div>
+    <div class="header-bottom">
+        <div class="header-wrapper">
+            <nav class="header-menu">
+                <ul>
+                    <li><a class="header-menu-link" href="limit:8080">Межкомнатные двери</a></li>
+                    <li><a class="header-menu-link" href="limit:8080">Стеклянные двери</a></li>
+                    <li><a class="header-menu-link" href="limit:8080">Системы открывания</a></li>
+                    <li><a class="header-menu-link" href="limit:8080">Фурнитура</a></li>
+                    <li><a class="header-menu-link" href="limit:8080">Комплектующий</a></li>
+                    <li>
+                        <a class="header-menu-link header-menu-link_special" href="limit:8080">
+                            <img class="header-menu-link-logo" src="icons/009-scale.svg">Бесплатный замер</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header-search">
+                <img class="header-search-icon" src="icons/005-search-interface-symbol.svg">
+            </div>
+        </div>
+    </div>
+</header>
