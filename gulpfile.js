eeaec04e0165aeb4265e8aebae5fd85f3459d471
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-11-22 21:11:41 +0300

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,36 @@
-const gulp = require("gulp");
-const bs = require("browser-sync").create();
+const { src, dest, watch, series, parallel, task } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 const fileInclude = require("gulp-file-include");
-const fileDel = require("del");
+const del = require("del");
 
-const projectFolder = "dist";
-const sourceFolder = "src";
-const path = {
-  build: {
-    html: projectFolder + "/",
-    css: projectFolder + "/css/",
-    js: projectFolder + "/js/",
-    img: projectFolder + "/img/",
-    fonts: projectFolder + "/fonts/",
-  },
-  src: {
-    html: sourceFolder + "/*.html",
-    css: sourceFolder + "/css/**/*.css",
-    js: sourceFolder + "/js/script.js",
-    img: sourceFolder + "/img/**/*.{jpg, png, svg}",
-    fonts: sourceFolder + "/fonts/*.ttf",
-  },
-  watch: {
-    html: sourceFolder + "/*.html",
-    css: sourceFolder + "/sass/**/*.sass",
-    js: sourceFolder + "/js/**/*.js",
-    img: sourceFolder + "/img/**/*.{jpg, png, svg}",
-  },
-  clean: "./" + projectFolder + "/",
-};
-
-function server(params) {
-  bs.init({
-    server: {
-      baseDir: "./" + projectFolder + "/",
-    },
-    port: 8080,
-    notify: false,
+function serve() {
+  browserSync.init({
+    server: "./dist",
   });
+
+  watch("src/**/*.html", html);
+  watch("src/css/**/*.css", css);
 }
 
-function watch() {
-  gulp.watch([path.watch.html], html);
-}
-
-function clean() {
-  return fileDel(path.clean);
+function clear() {
+  return del("dist");
 }
 
 function html() {
-  return gulp
-    .src(path.src.html)
+  return src("src/**/*.html")
     .pipe(fileInclude())
-    .pipe(gulp.dest(path.build.html))
-    .pipe(bs.stream());
+    .pipe(dest("dist"))
+    .pipe(browserSync.stream());
 }
 
 function css() {
-  return gulp
-    .src(path.src.css)
-    .pipe(gulp.dest(path.build.css))
-    .pipe(bs.stream());
+  return src("src/css/**/*.css")
+    .pipe(autoprefixer())
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
 }
 
-const build = gulp.series(html, css);
-const watch = gulp.parallel(clean, build, watch, server);
+exports.build = build = series(clear, html, css);
 
-exports.build = build;
-exports.default = watch;
+exports.dev = series(build, serve);
