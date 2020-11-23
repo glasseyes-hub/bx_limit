b5b42a1da6f26fce603c1c1f6abf303df6daee68
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-11-23 09:53:54 +0300

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
-const { src, dest, watch, series, parallel, task } = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
+const { src, dest, watch, series } = require("gulp");
 const browserSync = require("browser-sync").create();
-const fileInclude = require("gulp-file-include");
 const del = require("del");
+
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const pug = require("gulp-pug");
 
 function serve() {
   browserSync.init({
-    server: "./dist",
+    server: {
+      baseDir: "./dist",
+    },
   });
 
   watch("src/**/*.html", html);
-  watch("src/css/**/*.css", css);
+  watch("src/css/**/*.sass", css);
 }
 
 function clear() {
@@ -18,19 +22,38 @@ function clear() {
 }
 
 function html() {
-  return src("src/**/*.html")
-    .pipe(fileInclude())
+  return src("src/**/*.pug")
+    .pipe(pug())
     .pipe(dest("dist"))
     .pipe(browserSync.stream());
 }
 
 function css() {
-  return src("src/css/**/*.css")
+  return src("src/css/**/*.sass")
+    .pipe(sass())
     .pipe(autoprefixer())
     .pipe(dest("dist/css"))
     .pipe(browserSync.stream());
 }
 
-exports.build = build = series(clear, html, css);
+function fonts() {
+  return src("src/fonts/**/*.*")
+    .pipe(dest("dist/fonts"))
+    .pipe(browserSync.stream());
+}
+
+function img() {
+  return src("src/img/**/*.*")
+    .pipe(dest("dist/img"))
+    .pipe(browserSync.stream());
+}
+
+function icons() {
+  return src("src/icons/**/*.*")
+    .pipe(dest("dist/icons"))
+    .pipe(browserSync.stream());
+}
+
+exports.build = build = series(clear, html, css, fonts, img, icons);
 
 exports.dev = series(build, serve);
