d70e8a08ffbfb9f6028139d6c597f60bc27b8093
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-12-08 11:02:22 +0300

    Fix. Banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,17 +1,25 @@
 class Banner {
   position = 0;
-  intervalTimeout = 10000;
+  intervalTimeout = 3000;
   animationSpeed = 1500;
 
   constructor(element) {
     this.container = $(element);
     this.info = this.container.find($(".banner-info"));
     this.images = this.container.find($(".banner-img"));
-    this.points = this.container.find($(".banner-point"));
+    this.points = this.container.find($(".banner-points"));
     this.bannersAmount = this.images.length;
 
+    this.init();
+  }
+
+  init() {
+    this.info.each((index, element) => $(element).hide());
+    this.images.each((index, element) => $(element).hide());
+    this.addPoints();
+
+    this.swap();
     this.setInterval();
-    this.handlers();
   }
 
   setInterval() {
@@ -25,7 +33,7 @@ class Banner {
   swap() {
     if (this.position === this.bannersAmount) this.position = 0;
 
-    this.unselectData();
+    this.unselectData(this.position - 1);
     this.selectData(this.position);
     this.selectPoint(this.position);
 
@@ -44,18 +52,29 @@ class Banner {
     });
   }
 
+  addPoints() {
+    for (let index = 0; index < this.bannersAmount; index++) {
+      const point = document.createElement("div");
+      $(point).addClass("banner-point");
+
+      if (index === 0) $(point).addClass("banner-point_selected");
+
+      this.points.append(point);
+    }
+  }
+
   selectPoint(index) {
-    this.points.each((i, point) => {
+    this.points.children().each((i, point) => {
       if (index === i) $(point).addClass("banner-point_selected");
       else $(point).removeClass("banner-point_selected");
     });
   }
 
-  unselectData() {
-    for (let index = 0; index <= this.bannersAmount; index++) {
-      $(this.info[index]).slideUp({ duration: this.animationSpeed / 2 });
-      $(this.images[index]).fadeOut({ duration: this.animationSpeed / 2 });
-    }
+  unselectData(index) {
+    index = index < 0 ? this.bannersAmount - 1 : index;
+
+    $(this.info[index]).slideUp({ duration: this.animationSpeed / 2 });
+    $(this.images[index]).fadeOut({ duration: this.animationSpeed / 2 });
   }
 
   selectData(index) {
