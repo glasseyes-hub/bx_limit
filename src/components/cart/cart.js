6ea62068cc6d4fe46994dc5699fc77750797996f
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-12-08 17:17:04 +0300

    Fix. Cart

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,22 +1,39 @@
 class Cart {
   animationSpeed = 300;
+  isOpen = false;
+  autoCloseSpeed = 1000;
 
   constructor(container) {
     this.container = container;
     this.details = this.container.find($(".cart-details"));
 
     this.details.hide();
+    this.setHandler();
+  }
 
+  setHandler() {
     this.container.hover(
       () => {
-        this.container.addClass("cart_open");
-        this.details.show(this.animationSpeed);
+        if (!this.isOpen) {
+          this.container.addClass("cart_open");
+          this.details.fadeIn(this.animationSpeed);
+          this.isOpen = true;
+        } else {
+          clearTimeout(this.closeTimeout);
+        }
       },
       () => {
-        this.container.removeClass("cart_open");
-        this.details.hide(this.animationSpeed);
+        this.setCloseTimeout();
       }
     );
+  }
+
+  setCloseTimeout() {
+    this.closeTimeout = setTimeout(() => {
+      this.container.removeClass("cart_open");
+      this.details.fadeOut(this.animationSpeed);
+      this.isOpen = false;
+    }, this.autoCloseSpeed);
   }
 }
 
