710a6236eca9804097e49d5449e56d36aaee0ca1
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-12-08 13:21:56 +0300

    Ref. Banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,55 +1,44 @@
 class Banner {
-  position = 0;
+  start = 0;
   intervalTimeout = 3000;
   animationSpeed = 1500;
 
   constructor(element) {
     this.container = $(element);
-    this.info = this.container.find($(".banner-info"));
-    this.images = this.container.find($(".banner-img"));
+    this.descriptions = this.container
+      .find($(".banner-descriptions"))
+      .children();
+    this.images = this.container.find($(".banner-images")).children();
     this.points = this.container.find($(".banner-points"));
-    this.bannersAmount = this.images.length;
 
     this.init();
   }
 
+  set position(position) {
+    this._position = position >= this.bannersAmount ? 0 : position;
+  }
+  get position() {
+    return this._position;
+  }
+
+  set nextPosition(position) {
+    this._nextPosition = position >= this.bannersAmount ? 0 : position;
+  }
+  get nextPosition() {
+    return this._nextPosition;
+  }
+
   init() {
-    this.info.each((index, element) => $(element).hide());
+    this.descriptions.each((index, element) => $(element).hide());
     this.images.each((index, element) => $(element).hide());
+    this.bannersAmount = this.images.length;
+    this.nextPosition = this.start;
+    this.position = this.nextPosition + 1;
+
     this.addPoints();
-
-    this.swap();
+    this.addHandlers();
+    this.switch();
     this.setInterval();
-  }
-
-  setInterval() {
-    this.interval = setInterval(() => this.swap(), this.intervalTimeout);
-  }
-
-  clearInterval() {
-    clearInterval(this.interval);
-  }
-
-  swap() {
-    if (this.position === this.bannersAmount) this.position = 0;
-
-    this.unselectData(this.position - 1);
-    this.selectData(this.position);
-    this.selectPoint(this.position);
-
-    this.position++;
-  }
-
-  handlers() {
-    this.points.each((index, point) => {
-      $(point).on("click", () => {
-        this.position = index;
-
-        this.clearInterval();
-        this.swap();
-        this.setInterval();
-      });
-    });
   }
 
   addPoints() {
@@ -57,29 +46,60 @@ class Banner {
       const point = document.createElement("div");
       $(point).addClass("banner-point");
 
-      if (index === 0) $(point).addClass("banner-point_selected");
-
       this.points.append(point);
     }
   }
 
-  selectPoint(index) {
-    this.points.children().each((i, point) => {
-      if (index === i) $(point).addClass("banner-point_selected");
-      else $(point).removeClass("banner-point_selected");
+  addHandlers() {
+    this.points.children().each((index, point) => {
+      $(point).on("click", () => {
+        this.nextPosition = index;
+
+        clearInterval(this.interval);
+        this.switch();
+        this.setInterval();
+      });
     });
   }
 
-  unselectData(index) {
-    index = index < 0 ? this.bannersAmount - 1 : index;
-
-    $(this.info[index]).slideUp({ duration: this.animationSpeed / 2 });
-    $(this.images[index]).fadeOut({ duration: this.animationSpeed / 2 });
+  setInterval() {
+    this.interval = setInterval(() => this.switch(), this.intervalTimeout);
   }
 
-  selectData(index) {
-    $(this.info[index]).slideDown({ duration: this.animationSpeed });
-    $(this.images[index]).fadeIn({ duration: this.animationSpeed });
+  switch() {
+    this.switchBanner();
+    this.switchPoint();
+
+    this.position = this.nextPosition;
+    this.nextPosition++;
+  }
+
+  switchBanner() {
+    this.hideBanner(this.position);
+    this.showBanner(this.nextPosition);
+  }
+
+  switchPoint() {
+    this.unselectPoint(this.position);
+    this.selectPoint(this.nextPosition);
+  }
+
+  selectPoint(index) {
+    this.points.children().eq(index).addClass("banner-point_selected");
+  }
+
+  unselectPoint(index) {
+    this.points.children().eq(index).removeClass("banner-point_selected");
+  }
+
+  showBanner(index) {
+    this.descriptions.eq(index).slideDown({ duration: this.animationSpeed });
+    this.images.eq(index).fadeIn({ duration: this.animationSpeed });
+  }
+
+  hideBanner(index) {
+    this.descriptions.eq(index).slideUp({ duration: this.animationSpeed / 2 });
+    this.images.eq(index).fadeOut({ duration: this.animationSpeed / 2 });
   }
 }
 
