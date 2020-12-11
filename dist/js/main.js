d0f382ee6298f4c829c93b6b19c2dbe4791f5f37
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-12-11 13:49:32 +0300

    Ref. ShowAll

--- dist/js/main.js
+++ dist/js/main.js
@@ -300,12 +300,17 @@ $(".filterItem").each((index, element) => {
 
 class ShowAll {
   isShowed = false;
+  hideHeight = "150px";
+  openHeight = null;
+
   constructor(container) {
     this.container = container;
     this.body = this.container.find($(".showAll-body"));
     this.button = this.container.find($(".showAll-button"));
+    this.openHeight = this.body.height();
 
     this.addHandlers();
+    this.hide();
   }
 
   addHandlers() {
@@ -316,12 +321,14 @@ class ShowAll {
   }
 
   hide() {
-    this.body.animate({ height: "150px" });
+    this.body.animate({ height: this.hideHeight });
+    this.button.text("Показать все");
     this.isShowed = false;
   }
 
   show() {
-    this.body.animate({ height: "auto" });
+    this.body.animate({ height: this.openHeight });
+    this.button.text("Свернуть");
     this.isShowed = true;
   }
 }
--- src/layouts/showAll/showAll.js
+++ src/layouts/showAll/showAll.js
@@ -1,11 +1,16 @@
 class ShowAll {
   isShowed = false;
+  hideHeight = "150px";
+  openHeight = null;
+
   constructor(container) {
     this.container = container;
     this.body = this.container.find($(".showAll-body"));
     this.button = this.container.find($(".showAll-button"));
+    this.openHeight = this.body.height();
 
     this.addHandlers();
+    this.hide();
   }
 
   addHandlers() {
@@ -16,12 +21,14 @@ class ShowAll {
   }
 
   hide() {
-    this.body.animate({ height: "150px" });
+    this.body.animate({ height: this.hideHeight });
+    this.button.text("Показать все");
     this.isShowed = false;
   }
 
   show() {
-    this.body.animate({ height: "auto" });
+    this.body.animate({ height: this.openHeight });
+    this.button.text("Свернуть");
     this.isShowed = true;
   }
 }
